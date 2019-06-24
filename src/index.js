1052a1756004cbfc14ca066bc77d5d62f5aeab82
AgathaP <ebrumaktub@gmx.com>
2019-06-23 23:32:17 -0400

    ordeno elementos dentro de archivo correspondiente y limino comentarios

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const marked = require('marked')
 const fs = require('fs');
 const fileHound = require('filehound');
 const path = require('path');
+const file = process.argv[2];
 // const fetch = require('node-fetch');
 
 // import { fstat } from "fs";
@@ -14,6 +15,7 @@ const path = require('path');
 
 
 // Función que recorre una ruta e imprime en terminal, dentro de un array, los resultados de la busqueda.
+
     const files = fileHound.create()
         // .paths(path) // es la ruta (cómo sacarla es el punto, hasta el momento la he escrito manual)
         .paths('\\Users\\Asus\\Desktop\\SCL009-md-links')
@@ -24,16 +26,42 @@ const path = require('path');
             console.log('Por favor ingrese una ruta valida');
     })  
 
+
+// Función que comprueba que las rutas sean absolutas, de lo contrario las convierte en absolutas.
+
     // if(path.isAbsolute(files) !== true){
     //     console.log('es una ruta relativa');
     //     } else {
     //         console.log('es una ruta absoluta')
     //     }
         
-    // intento de entrada a los archivos
-// const enterArchive = (files) => {
-//     new Promise()
-//     .then(){
 
-//     }
-// }
+// Función que lee archivo, lo muestra en terminal y arroja los links encontrados en el, con 
+// su descripción y ruta.
+    
+// const read = (path) =>{
+  fs.readFile(file, 'utf8', ( err, data ) => {
+    console.log(data); 
+
+    if(err) {
+      console.log(err);
+    }
+
+      let arr = [];
+      const renderer = new marked.Renderer();
+      renderer.link = function (href, text, file) {
+          arr.push({    
+            // Url encontrada.
+              href: href,
+            // Texto que aparecería dentro del link.
+              text: file,
+            // Ruta del archivo donde se encontró el link.
+              file: process.argv[1],
+              });
+              console.log(arr) 
+            };
+
+      marked(data, {renderer:renderer})
+  
+  });
+  // };
