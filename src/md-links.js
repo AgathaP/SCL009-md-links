3d3fbbb5e00adf035422e7bbb72582d43d013dcc
AgathaP <ebrumaktub@gmx.com>
2019-07-06 10:53:41 -0400

    En md-links.js hago ondicional que sólo permite mostrar archivos en formato markdow, de lo contrario imprime un error

--- src/md-links.js
+++ src/md-links.js
@@ -1,15 +1,19 @@
-const mdLinks = require('./index.js')
 // Modulos exportados en este proyecto
+const mdLinks = require('./index.js')
 const path = require('path');
 const fs = require('fs');
 // Variable global
 let route = process.argv[2];
+let pathExtname = route.split('.');
+let extname = pathExtname.pop();
+
 
 // Si la ruta es relativa la normaliza y convierte en absoluta.
 if (!path.isAbsolute(route)) {
   route = path.normalize(route)
   route = path.resolve(route)
 }
+
 // Retorna una lista de archivos en caso de ser un directorio,
 // de lo contrario, retorna una lista con los links encontrados
 // dentro de un archivo.
@@ -18,15 +22,14 @@ if(fs.lstatSync(route).isDirectory()) {
     .then(res => {
       console.log(res);
     })
-} else {
+  }
+  
+// verifica que el archivo esté en formato Markdown
+if (fs.lstatSync(route).isFile() && extname === 'md') { 
   mdLinks.searchingLinks(route)
     .then(res => {
       console.log(res);
     })
+} else {
+  console.log('Este archivo no está en formato Markdown')
 }
-// me retorna igual los archivos(por ende recorre el directorio(?) necesito el forEach)
-
-// 
-// if(){
-
-// }
